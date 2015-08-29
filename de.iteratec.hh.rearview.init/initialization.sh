1c688a3c967e5eb87964dc01aae25fe93c779ec8
Nils Kuhn <nils.kuhn@iteratec.de>
2015-08-29 07:08:53 +0200

    set db connection string in rearview config on startup

--- de.iteratec.hh.rearview.init/initialization.sh
+++ de.iteratec.hh.rearview.init/initialization.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+LINKED_MYSQL_CONTAINER="rearview-mysql"
+
 # preparation of mysql database
 
-RESULT=`mysql -h rearview-mysql -uroot -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE'"`
+RESULT=`mysql -h $LINKED_MYSQL_CONTAINER -uroot -p$MYSQL_ROOT_PASSWORD --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE'"`
 if [ "$RESULT" == "$MYSQL_DATABASE" ]; then
     echo "Rearview database already exists -> nothing to do here."
 else
 	echo "Rearview database does not exist -> Creating database named $MYSQL_DATABASE."
 	Q1="CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-	mysql -h rearview-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "$Q1"
+	mysql -h $LINKED_MYSQL_CONTAINER -uroot -p$MYSQL_ROOT_PASSWORD -e "$Q1"
 	echo "Rearview database does not exist -> Creating database user named $MYSQL_USER and grant access to db."
 	Q2="GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 	Q3="FLUSH PRIVILEGES;"
-	mysql -h rearview-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "${Q2}${Q3}"
+	mysql -h $LINKED_MYSQL_CONTAINER -uroot -p$MYSQL_ROOT_PASSWORD -e "${Q2}${Q3}"
 	echo "Rearview database does not exist -> Import dump."
-	mysql -h rearview-mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /tmp/mysql-dumps/initial-mysql-dump.sql
+	mysql -h $LINKED_MYSQL_CONTAINER -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /tmp/mysql-dumps/initial-mysql-dump.sql
 fi
 
 # set rearview-config
 
-sed -i -r 's/graphite\.host=.+/graphite.host="http:\/\/monitoring.hh.iteratec.de"/g' common.conf
+echo "set graphite.host in common.conf to $GRAPHITE_HOST"
+sed -i -r 's/graphite\.host=.+/graphite.host="http:\/\/$GRAPHITE_HOST"/g' common.conf
+echo "set db.default.user to $MYSQL_USER"
 sed -i -r 's/db\.default\.user=.+/db.default.user="$MYSQL_USER"/g' common.conf
+echo "set db.default.password to ***"
 sed -i -r 's/db\.default\.password=.+/db.default.password="$MYSQL_PASSWORD"/g' common.conf
+echo "set db.default.url to jdbc:mysql://$LINKED_MYSQL_CONTAINER:3306/$MYSQL_DATABASE"
+sed -i -r 's/db\.default\.url=.+/db.default.url="jdbc:mysql://$LINKED_MYSQL_CONTAINER:3306/$MYSQL_DATABASE"/g' common.conf
