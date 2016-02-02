c8f466a0a988ee10a9065f6a07943a22b3d4bc0c
Hauke Mettendorf <hauke.mettendorf@iteratec.de>
2016-02-02 11:40:30 +0100

    Edited templates

--- de.iteratec.hh.osm/templates/osm-config.groovy
+++ de.iteratec.hh.osm/templates/osm-config.groovy
@@ -1,10 +1,10 @@
 //base url of application
-grails.serverURL = "http://${BASE_URL_OSM}"
+grails.serverURL = "http://%BASE_URL_OSM%"
 // Configure default app users
-grails.de.iteratec.osm.security.initialOsmAdminUser.username='${OSM_ADMIN_USER}'
-grails.de.iteratec.osm.security.initialOsmAdminUser.password='${OSM_ADMIN_PASSWORD}'
-grails.de.iteratec.osm.security.initialOsmRootUser.username='${OSM_ROOT_USER}'
-grails.de.iteratec.osm.security.initialOsmRootUser.password='${OSM_ROOT_PASSWORD}'
+grails.de.iteratec.osm.security.initialOsmAdminUser.username='%OSM_ADMIN_USER%'
+grails.de.iteratec.osm.security.initialOsmAdminUser.password='%OSM_ADMIN_PASSWORD%'
+grails.de.iteratec.osm.security.initialOsmRootUser.username='%OSM_ROOT_USER%'
+grails.de.iteratec.osm.security.initialOsmRootUser.password='%OSM_ROOT_PASSWORD%'
 //configure charting library
 grails.de.iteratec.osm.report.chart.chartTagLib = de.iteratec.osm.report.chart.ChartingLibrary.RICKSHAW
 grails.de.iteratec.osm.report.chart.availableChartTagLibs = [de.iteratec.osm.report.chart.ChartingLibrary.RICKSHAW]
@@ -13,9 +13,9 @@ environments {
 	production {
 		dataSource {
 			dbCreate = "update"// one of 'create', 'create-drop', 'update', 'validate', ''
-			url = "jdbc:mysql://osm-mysql/${MYSQL_DATABASE}"
-			username = "${MYSQL_USER}"
-			password = "${MYSQL_PASSWORD}"
+			url = "jdbc:mysql://osm-mysql/%MYSQL_DATABASE%"
+			username = "%MYSQL_USER%"
+			password = "%MYSQL_PASSWORD%"
 			driverClassName = "com.mysql.jdbc.Driver"
 			logSql = false
 			pooled = true
