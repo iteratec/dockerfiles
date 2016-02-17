f7029602a9a4041ca345471475fa63a1b4fd7096
BirgerK <birger.kamp@iteratec.de>
2016-02-17 14:15:20 +0100

    [jenkinsslave] fix java

--- de.iteratec.hh.jenkins-slave/start_script.sh
+++ de.iteratec.hh.jenkins-slave/start_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
-echo $SSH_PUB_KEY >> /home/jenkins/.ssh/authorized_keys
+echo "$SSH_PUB_KEY" >> /home/jenkins/.ssh/authorized_keys
 
 /usr/sbin/sshd -D
