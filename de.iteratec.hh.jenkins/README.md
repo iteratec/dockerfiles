48d69ddab5bdf26c0a66f39657b012aeaa1c2433
Thomas Steinbach <thomas.steinbach@aikq.de>
2016-04-28 05:28:46 +0200

    further dockerizing of ACI

--- de.iteratec.hh.jenkins/README.md
+++ de.iteratec.hh.jenkins/README.md
@@ -2,11 +2,30 @@
 
 This is a library/jenkins with an additional Ansible installed. It is used for ansible-ci.
 
-## run
+## 1 Configuration
+
+Get the example configuration and modify according your needs:
 
 ```
-docker run -d --name aci -p 8081:8080
-  -v ~/git/hh.iteratec.de/ansible-infrastructure/playbooks:/var/jenkins_home/workspace/develop/defaultPlaybooks
-  -v ~/git/hh.iteratec.de/ansible-infrastructure/roles:/var/jenkins_home/workspace/develop/defaultRoles
+docker run --rm -v "$(pwd)":/config iteratechh/jenkins cp -a /example_config /config
+mv example_config ansible_config
+```
+
+## 2 Start ACI with configuration
+
+```
+#!/bin/bash
+echo 'Vault Password: '
+read -s avp
+docker run -d --name aci -p 8081:8080 --env "ANSIBLE_VAULT_PASSWORD=$avp"
+  -v /path/to/ansible_config:/ansible_config
+  -v /path/to/repository:/var/jenkins_home/workspace/develop/<repo-label>
   iteratechh/jenkins
+
+```
+
+## 3 Deploy ACI Agent
+
+```
+docker exec -it aci deploy-agents
 ```
--- de.iteratec.hh.jenkins/files/system/start.sh
+++ de.iteratec.hh.jenkins/files/system/start.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
+set -e
+
 /usr/local/bin/jenkins.sh date # pass 'date' to suppress startup of jenkins
-
-source /ansible/hacking/env-setup
-ansible --version
-
-cd /ansible_data/playbook
-
-echo "$ANSIBLE_VAULT_PASSWORD" > /tmp/ansible_vaultpass
-ansible-playbook --vault-password-file /tmp/ansible_vaultpass site.yml
-
-# the following lines are from the docker images original /usr/local/bin/jenkins.sh
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+
+  if [[ $(ls /ansible_config/) ]]; then
+    cp /ansible_config/* /example_config
+  fi
+
+  echo "$ANSIBLE_VAULT_PASSWORD" > /tmp/ansible_vaultpass
+  cd /ansible_data/playbooks/setup
+  ansible-playbook --vault-password-file /tmp/ansible_vaultpass site.yml
+
    exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
 fi
 
-# As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
 exec "$@"
